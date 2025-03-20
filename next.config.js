fbe19fb5a8d5cbe6f8b4d29f8021af72f2f08285
PavanBhatt07 <pavanbhatsmg@gmail.com>
2025-03-20 22:51:11 +0530

    r

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // Enables static export (Required for Netlify)
   images: {
-    unoptimized: true, // Fixes images not loading on Netlify
     remotePatterns: [
       { protocol: "https", hostname: "images.unsplash.com" }, 
-      { protocol: "https", hostname: "i.imgur.com" },  
-      { protocol: "https", hostname: "drive.google.com" }  
+      { protocol: "https", hostname: "i.imgur.com" },  // Add Imgur
+      { protocol: "https", hostname: "drive.google.com" } // Add Google Drive
     ],
   },
   experimental: {
